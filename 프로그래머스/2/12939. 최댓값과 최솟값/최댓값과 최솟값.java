ebdf5f571c08f0f0ec12e881190733cec87083f2
Yerin Jung <dpfls7582@gmail.com>
2024-07-10 15:18:36 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 9.29 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,29 +1,14 @@
 import java.util.*;
+
 class Solution {
     public String solution(String s) {
         String answer = "";
-        //문자열 공백 기준으로 나누기
-        String[] array = s.split(" ");
-        //정수 배열 선언 (길이 정의 필요)
-        int[] int_array = new int[array.length];
-        //정수 배열에 문자열 넣기 [1,2,3,4]
-        for(int i=0; i<array.length; i++){
-            int_array[i] = Integer.parseInt(array[i]);
-        }
-        //최대, 최소 추출
-        Arrays.sort(int_array);
-        int min=int_array[0];
-        int max=int_array[0];
-        for(int i=0; i<int_array.length; i++){
-            for(int j=0; j<i; j++){
-                if(int_array[i]<=int_array[j]){
-                    min = int_array[i];
-                }else if(int_array[i]>=int_array[j]){
-                    max = int_array[i];
-                }
-            }
-        }
-        answer = min + " " + max;
+        int[] s_list = Arrays.stream(s.split(" "))
+                         .mapToInt(Integer::parseInt)
+                        .toArray();
+        Arrays.sort(s_list);
+        
+        answer = s_list[0] + " " +s_list[s_list.length - 1];
         return answer;
     }
 }
