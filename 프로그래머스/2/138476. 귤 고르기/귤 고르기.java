09bb9703ae2ba6dcaa557ab3a0d7a60a1874e38e
Yerin Jung <dpfls7582@gmail.com>
2024-07-14 20:58:42 +0900

    [level 2] Title: 귤 고르기, Time: 95.35 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -4,32 +4,20 @@ import java.util.stream.Collectors;
 class Solution {
     public int solution(int k, int[] tangerine) {
         //귤 크기에 따른 개수 저장 : HashMap
-        HashMap<Integer, Integer> h = new HashMap<Integer, Integer>();
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
         for (int i=0; i<tangerine.length; i++){
-            int x = 1;      // 빈도수 카운트 시작 = 1 (무조건 하나는 있으니까)
-            if(h.containsKey(tangerine[i])){
-                x = h.get(tangerine[i]) + 1;
+            int x = 1;          // 빈도수 카운트 시작 = 1 (무조건 하나는 있으니까)
+            if(map.containsKey(tangerine[i])){
+                x = map.get(tangerine[i]) + 1;
             }
-            h.put(tangerine[i], x);
+            map.put(tangerine[i], x);
         }
-            
-        //value 값 기준으로 내림차순 정렬
-        HashMap<Integer, Integer> h_sorted = h.entrySet()
-                                              .stream()
-                                              .sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed())
-                                              .collect(Collectors.toMap(
-                                                  Map.Entry::getKey,
-                                                  Map.Entry::getValue,
-                                                  (e1, e2) -> e1,
-                                                  LinkedHashMap::new
-                                              ));
-
-        
-        //k에서 value 값을 빼면서 0이 되면 탈출
-        int answer = 0;                        //key의 고유한 개수
-        for (int key : h_sorted.keySet()){     //key 값
-            int value = h_sorted.get(key);     //value 값
-            k -= value;
+        //value 값 기준으로 내림차순 정렬 : List
+        List<Integer> list = new ArrayList<>(map.keySet());
+        list.sort((o1, o2) -> map.get(o2) - map.get(o1));
+        int answer = 0;         //key의 고유한 개수
+        for(Integer key : list){
+            k -= map.get(key);  //value값 빼기
             answer += 1;
             if(k <= 0){
                 break;
