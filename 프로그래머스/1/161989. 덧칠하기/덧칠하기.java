0fdd958baeab68accbddceda5c68ee4c4b37708c
Yerin Jung <dpfls7582@gmail.com>
2024-07-15 17:07:43 +0900

    [level 1] Title: 덧칠하기, Time: 2.23 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,24 +1,11 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
-        int answer = 0;
-        //1(칠한 구역)로 구성된 n 배열 생성
-        int[] wall = new int[n];
-        for(int i=0; i<n; i++){
-            wall[i] = 1;
-        }
-        //section 인덱스는 0(칠해야 되는 구역)으로 변환
-        for(int j : section){
-            wall[j-1] = 0;
-        }
-        //m만큼 1로 변환, 순회하면서 0일 경우 확인하기
-        
-        for(int k=0; k<section.length; k++){
-            int where = section[k]-1;
-            if(wall[where]==0){
-                for(int p = where; p < where + m && p < n; p++){
-                    wall[p] = 1;
-                }
+        int answer = 1;                 //무조건 덧칠 한번 시작
+        int start = section[0];         //시작 지점
+        for(int i=1; i<section.length; i++){
+            if(section[i] > start+m-1){     //덧칠한 마지막 인덱스보다 크면 칠하기
                 answer++;
+                start = section[i];     //시작 지점을 해당 인덱스로 재설정
             }
         }
         return answer;
