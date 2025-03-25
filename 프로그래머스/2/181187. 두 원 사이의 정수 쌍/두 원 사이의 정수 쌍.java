bf190f1a7fb79d7aeb5a32af60418f115447662d
Yerin Jung <dpfls7582@gmail.com>
2025-03-25 21:28:45 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 24.86 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
@@ -2,7 +2,7 @@ class Solution {
     public long solution(int r1, int r2) {
         long answer = 0;
         for(int i=1; i<=r2; i++){
-            //r1 기준 : i가 r1보다 작으면 (floor(r2의 y좌표)-ceil(r1의 y좌표))의 제곱근+1
+            //r1 기준 : i가 r1보다 작으면 floor(r2의 y좌표)-ceil(r1의 y좌표)+1
             //r2의 y좌표 = Math.sqrt(Math.pow(r2,2)-Math.pow(i,2))
             if(i<r1){
                 answer += Math.floor(Math.sqrt(Math.pow(r2,2)-Math.pow(i,2)))-Math.ceil(Math.sqrt(Math.pow(r1,2)-Math.pow(i,2)))+1;
