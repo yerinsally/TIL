f5277ca5f9e58d6c9a0ece86a3db895e96e35da6
Yerin Jung <dpfls7582@gmail.com>
2025-03-27 22:06:37 +0900

    [level 2] Title: 기능개발, Time: 2.80 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,32 +1,30 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        ArrayList<Integer> list = new ArrayList<>();
-        //큐 : 선입선출
-        Queue<Integer> queue = new LinkedList<>();
+        //1. 큐(선입선출)에 각각 개발 일수 넣기
+        Queue<Integer> q = new LinkedList<>();
         for(int i=0; i<progresses.length; i++){
-            int left = 100-progresses[i];
-            int a = left/speeds[i];
-            if(left%speeds[i] == 0){
-                queue.add(a);
-            } else {
-                queue.add(a+1);
+            if((100-progresses[i])%speeds[i]==0){
+                q.add((100-progresses[i])/speeds[i]);
+            }else{
+                q.add((100-progresses[i])/speeds[i]+1);
             }
         }
-        //System.out.print(queue);
-        while(!queue.isEmpty()){
-            int current = queue.poll();
+        //2. 조건에 따라 큐 poll
+        List<Integer> list = new ArrayList<>();
+        while(!q.isEmpty()){
+            //answer에 넣을 횟수 초기화(처음에 무조건 poll하니까 1로)
             int cnt = 1;
-            while(!queue.isEmpty() && queue.peek() <= current){
-                queue.poll();
+            //선입선출대로 조건에 따라 개발일수 저장
+            int num = q.poll();
+            while(!q.isEmpty() && q.peek() <= num){
+                q.poll();
                 cnt++;
             }
+            //answer에 넣을 횟수 리스트에 저장
             list.add(cnt);
         }
-        int[] answer = new int[list.size()];
-        for (int i=0; i<list.size(); i++) {
-            answer[i] = list.get(i);
-        }
-         return answer;
-     }
- }
+        //3. ArrayList → int[] 변환
+        return list.stream().mapToInt(i->i).toArray();
+    }
+}
