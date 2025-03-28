e45e75204e9935f42ba8bae6c76a341b8f285f4f
Yerin Jung <dpfls7582@gmail.com>
2025-03-29 00:10:30 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,21 +1,21 @@
 import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
-        //key = 의상의 종류(이름), value = 의상의 종류(개수)
-        //해당 의상을 선택하지 않는 경우 포함하기!
-        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        //HashMap : key-value 쌍
+        //의상종류 - 의상이름의 개수 쌍
+        HashMap<String, Integer> hm = new HashMap<>();
         for(int i=0; i<clothes.length; i++){
-            if(map.containsKey(clothes[i][1])){
-                map.put(clothes[i][1], map.get(clothes[i][1])+1);
-            }else{
-                map.put(clothes[i][1], 1);
-            }
+            String key = clothes[i][1];
+            hm.put(key, hm.getOrDefault(key,0)+1);
         }
-        System.out.println(map);
-        int answer = 1;
-        for(String key : map.keySet()){
-            int number = map.get(key)+1;
-            answer *= number;
+        //HashMap 길이만큼 순회
+        //모든 value에 1을 더함(해당 의상 종류를 입지 않는 경우)
+        //의상 종류별 선택 횟수를 구하기 위해 곱하기
+        //모든 의상을 입지 않는 경우 제외
+        int answer = 1;     //최소 한개의 의상은 입음
+        for(String key : hm.keySet()){
+            int num = hm.get(key)+1;
+            answer *= num;
         }
         return answer-1;
     }
