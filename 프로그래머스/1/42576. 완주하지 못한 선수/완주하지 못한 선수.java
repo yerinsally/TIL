d8dc317a6674332e2c2d54a26ecf1e6dcf03c3cf
Yerin Jung <dpfls7582@gmail.com>
2025-03-28 23:18:19 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 94.27 ms, Memory: 97.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,17 +1,22 @@
 import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        Map<String, Integer> map = new HashMap<String, Integer>();
+        //HashMap : key-value 쌍
+        HashMap<String, Integer> hm = new HashMap<>();
         for(String p : participant){
-            map.put(p, map.getOrDefault(p,0)+1);
+            hm.put(p, hm.getOrDefault(p,0)+1);
         }
+        //HashMap 갱신 : 완주했으면 key의 value-1
         for(String c : completion){
-            map.put(c, map.get(c)-1);   //완료한 선수는 value-1
+            if(hm.containsKey(c)){
+                hm.put(c, hm.get(c)-1);
+            }
         }
-        for(String m : participant){    //for문 : participant에 있는 선수들에 대해
-            if(map.get(m)>0){
-                answer = m;
+        //완주하지 못한 선수 = 1 이상의 값을 가지고 있음
+        String answer = "";
+        for(String key : hm.keySet()){
+            if(hm.get(key)>0){
+                answer = key;
             }
         }
         return answer;
