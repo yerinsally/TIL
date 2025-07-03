851851bf9fa13ce993ce071f955ebd2bdacc2573
Yerin Jung <dpfls7582@gmail.com>
2025-07-04 04:07:08 +0900

    [level 1] Title: K번째수, Time: 1.23 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,18 +2,16 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        for(int i=0; i<commands.length; i++){
-            int left = commands[i][0]-1;     //index = 1
-            int right = commands[i][1]-1;    //index = 4
-            int k = commands[i][2]-1;        //index = 2
-            
-            int len = right-left+1;          //자른 배열 길이 = 4
-            int[] sub = new int[len];
-            for(int j=left; j<=right; j++){  //1부터 4까지 순회
-                sub[j-left] = array[j];      //자른 배열의 0~3 인덱스 = j-left
-            }
-            Arrays.sort(sub);
-            answer[i] = sub[k];
+        int i=0, j=0, k=0;
+        for (int a=0 ; a<commands.length; a++) {
+            //[i,j,k] : 인덱스이므로 i-1,j-1
+            i = commands[a][0]-1; 
+            j = commands[a][1];
+            k = commands[a][2];
+            //배열 복사 : Arrays.copyOfRange(배열,i부터,j-1까지)
+            int[] new_arr = Arrays.copyOfRange(array,i,j);
+            Arrays.sort(new_arr);
+            answer[a] = new_arr[k-1];
         }
         return answer;
     }
